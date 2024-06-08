171f4fe68c4f12f613445345d0faa2e1d74a2a64
ezio <ezio.xie@binance.com>
2024-06-08 19:25:39 +0100

    go

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
     const imageLinks = [
-        '/1.png','/2.png','/3.png','/4.png','/5.png','/6.png'
+        '/tofu.png','/person.png',
+        '/card.png','/2.png','/3.png','/4.png',
+    ]
+    const socials = [
+        {name: 'Facebook', link: 'https://www.facebook.com/tsenoodle'},
+        {name: 'yelp', link: "https://www.yelp.com/biz/tse-noodles-oxford"},
+        {name: 'Twitter', link: 'https://twitter.com/tse.noodle'},
     ]
 </script>
 <div class="text-4xl">Welcome to Tse Noodle</div>
 <div>Best Chinese Eatery in the heart of Oxford</div>
-<div class="grid grid-cols-2">
+<div class="flex gap-1 justify-evenly w-full">
+    {#each socials as social}
+        <a href={social.link} target="_blank">{social.name}</a>
+    {/each}
+</div>
+<div class="grid grid-cols-2 gap-2">
     {#each imageLinks as imageLink}
         <div>
             <img src={imageLink} alt="Tse Noodle" />
