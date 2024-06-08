b78770b8a19a3e3988899e28cbd6c51ba414385f
ezio <ezio.xie@binance.com>
2024-06-08 19:08:47 +0100

    go

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,4 @@
-<slot></slot><script>import "../app.css";</script><style></style>
+<div class="w-full h-full flex flex-col justify-center items-center p-2">
+    <slot />
+</div>
+<script>import "../app.css";</script><style></style>
